71360d4063ea0a4ff5f9c010e9388384260f2905
Alistair Miles <alimanfoo@googlemail.com>
2016-01-21 14:00:35 +0000

    fix basemap build

--- install/basemap.sh
+++ install/basemap.sh
@@ -4,7 +4,7 @@ set -xeuo pipefail
 
 cd /biipy
 
-export GEOS_DIR=/usr/local
+export GEOS_DIR=/usr
 curl -OL http://sourceforge.net/projects/matplotlib/files/matplotlib-toolkits/basemap-1.0.7/basemap-1.0.7.tar.gz
 tar -xzf basemap-1.0.7.tar.gz
 cd basemap-1.0.7
